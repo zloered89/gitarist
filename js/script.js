72537439fe6224b89d62b6ed1da68382b431a559
zloered89 <116219036+zloered89@users.noreply.github.com>
2022-12-20 17:59:02 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.nav-menu'),
+    menuItem = document.querySelectorAll('.nav-menu__list-item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('nav-menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('nav-menu_active');
+        })
+    })
+})
+
+
+
+
+  $(document).ready(function(){
+  $("a[href*='#']").on("click", function(e){
+    var anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $(anchor.attr('href')).offset().top
+    }, 777);
+    e.preventDefault();
+    return false;
+  });
+});
